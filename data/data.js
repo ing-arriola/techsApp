bf48e4aaf49e5bb2db0e142c348b299db619153a
Jaime <ingeniero.arriola@gmail.com>
2021-05-08 09:26:57 -0600

    Mock data

---
+++ data/data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category"
+
+export const CATEGORIES = [
+    new Category('c1', 'React', '#f5428d'),
+    new Category('c2', 'CSS', '#f54242'),
+    new Category('c3', 'Flutter', '#f5a442'),
+    new Category('c4', 'Javascript', '#f5d142'),
+    new Category('c5', 'React Native', '#368dff'),
+    new Category('c6', 'Database', '#41d95d'),
+    new Category('c7', 'API', '#9eecff'),
+    new Category('c8', 'Java', '#b9ffb0'),
+    new Category('c9', 'Python', '#ffc7ff'),
+    new Category('c10', 'CI&CD', '#47fced')
+]
