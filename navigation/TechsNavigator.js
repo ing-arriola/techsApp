1b1507494727bd06dd9a895fbfce7d40ebd6827e
Jaime <ingeniero.arriola@gmail.com>
2021-05-08 10:00:33 -0600

    Added navigation options

--- navigation/TechsNavigator.js
+++ navigation/TechsNavigator.js
@@ -1,12 +1,23 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer  } from 'react-navigation'
+import Colors from '../constants/Colors'
 
 import CategoriesScreen from '../Screens/CategoriesScreen'
 import CategoriesTechsScreen from '../Screens/CategoriesTechsScreen'
 import TechDetailScreen from '../Screens/TechDetailScreen'
 
 const TechsNavigator = createStackNavigator({
-    Categories:CategoriesScreen,
+    Categories:{
+        screen:CategoriesScreen,
+        navigationOptions : {
+            headerTitle : 'Techs Categories',
+            headerStyle:{
+                backgroundColor:Colors.primaryColor
+            },
+            headerTintColor: "white",
+            headerTitleAlign:'center'
+        }
+    }, 
     CategoriesTechs:CategoriesTechsScreen,
     TechDetail:TechDetailScreen
 })
