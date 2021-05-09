f1905165d818ce353dfb634fb00bc46afb18317c
Jaime <ingeniero.arriola@gmail.com>
2021-05-09 16:35:06 -0600

    Optimized app

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font'
 import AppLoading  from 'expo-app-loading'
 import TechsNavigator from './navigation/TechsNavigator'
+import {enableScreens} from 'react-native-screens'
+
+enableScreens()
 
 const fetchFonts = () => {
   return Font.loadAsync({
