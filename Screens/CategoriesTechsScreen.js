b163c1750820c10b4ff5df06ee940914ef18dc86
Jaime <ingeniero.arriola@gmail.com>
2021-05-09 16:40:20 -0600

    Added navigation properties and a filter to display just techs that meets itemPased id

--- Screens/CategoriesTechsScreen.js
+++ Screens/CategoriesTechsScreen.js
@@ -1,20 +1,40 @@
 import React from 'react'
-import {View,Text,StyleSheet,Button} from 'react-native'
+import {View,Text,StyleSheet,Button,FlatList} from 'react-native'
+import {TECHS} from '../data/data'
+import Tech from '../models/Tech'
 
 const CategoriesTechsScreen = (props) => {
+    const itemPassed=props.navigation.getParam('item')
+    const techsToDisplay =  TECHS.filter(tech => tech.category.indexOf(itemPassed.id)>=0)
+
+    const renderTechItem = (itemData) => {
+        return(
+            <View>
+                <Text>
+                    {itemData.item.name}
+                </Text>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.screen} >
-            <Text>
-            CategoriesTechsScreen
-            </Text>
-            <Button title='Go to techDetail!' onPress={()=>{
-                props.navigation.navigate({
-                    routeName : 'TechDetail'
-                })
-            }}  />
+            <FlatList
+                data={techsToDisplay}
+                keyExtractor={(item,index) => item.id}
+                renderItem={renderTechItem}
+            />
         </View>
     )
 }
+
+CategoriesTechsScreen.navigationOptions = navigationData => { 
+    const itemPassed=navigationData.navigation.getParam('item')
+    return {
+        headerTitle : itemPassed.title,
+    }
+}
+
 
 const styles = StyleSheet.create({
     screen:{
@@ -25,3 +45,18 @@ const styles = StyleSheet.create({
 })
 
 export default CategoriesTechsScreen
+
+/*
+<Text>
+            CategoriesTechsScreen
+            </Text>
+            <Button title='Go to techDetail!' onPress={()=>{
+                props.navigation.navigate({
+                    routeName : 'TechDetail'
+                })
+            }}  />
+            <Text>
+                {itemPassed.title}
+            </Text>
+
+*/
