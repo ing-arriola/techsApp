2dab0a6a2f019cb1dabe7ebde5d6976889818f8a
Jaime <ingeniero.arriola@gmail.com>
2021-05-09 16:12:09 -0600

    Added a new model for techs

---
+++ models/Tech.js
@@ -0,0 +1,10 @@
+class Tech {
+    constructor(id,category,name,image){
+        this.id=id;
+        this.category=category;
+        this.name=name;
+        this.image=image;   
+    }
+}
+
+export default Tech
