14d626b65184fa9999a482e9fd121b1569ee63c5
Jaime <ingeniero.arriola@gmail.com>
2021-05-12 04:07:18 -0600

    Added markup and styles for image

--- components/CategoryGrid.js
+++ components/CategoryGrid.js
@@ -2,17 +2,21 @@ import React from 'react'
 import {TouchableOpacity,View,Text,StyleSheet,Image,Dimensions} from 'react-native'
 
 const CategoryGrid = ({itemData,onSelect}) => {    
-    const logo='../assets/techs/react.png'
-    console.log(typeof logo)
     return (
         <TouchableOpacity
             style={styles.gridItem} 
                 onPress={onSelect}
             > 
-                <View style={{...styles.container, ...{backgroundColor:itemData.item.color}}}>
+                <View style={{...styles.container, ...{backgroundColor:'rgba(0,0,0,0.20)'}}}>
                     <Text style={styles.title} >
                         {itemData.item.title}
                     </Text>
+                    <View style={styles.imageContainer} >
+                        <Image 
+                            style={styles.image}
+                            source={itemData.item.logo}
+                        />
+                    </View>
                 </View>
             </TouchableOpacity>
     )
@@ -33,6 +37,19 @@ const styles = StyleSheet.create({
     title:{
         fontSize:22,
         fontFamily:'open-sans'
+    },
+    imageContainer:{
+        width:Dimensions.get('window').width * 0.21,
+        height:Dimensions.get('window').width  * 0.21,
+        //borderRadius:Dimensions.get('window').width  * 0.25 /2,
+        //borderWidth:1,
+        //borderColor:'#ccc',
+        //overflow:'hidden',
+        marginVertical:5
+    },
+    image:{
+        width:'100%',
+        height:'100%',
     }
 })
 
