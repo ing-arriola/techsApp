12095965c3105b24ff07619d33d6082ef1749eef
Jaime <ingeniero.arriola@gmail.com>
2021-05-12 15:05:25 -0600

    Added button in header of techdetail screen so then the user will be able to mark an item as favorite

--- Screens/TechDetailScreen.js
+++ Screens/TechDetailScreen.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {View,Text,StyleSheet,Button} from 'react-native'
+import { HeaderButtons, Item  } from 'react-navigation-header-buttons'
+import CustomHButon from '../components/CustomHButon'
 
 const TechDetailScreen = (props) => {
+    console.log(props.navigation.getParam('item'))
     return (
         <View style={styles.screen} >
             <Text>
@@ -13,6 +16,22 @@ const TechDetailScreen = (props) => {
         </View>
     )
 }
+
+TechDetailScreen.navigationOptions = navigationData => { 
+    const itemPassed=navigationData.navigation.getParam('item')
+    return { 
+        headerTitle : itemPassed.name,
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={CustomHButon} >
+            <Item
+                iconName='star'
+                onPress={()=>{console.log('works!!')}}
+            />
+        </HeaderButtons>
+        )
+    }
+}
+
 
 const styles = StyleSheet.create({
     screen:{
