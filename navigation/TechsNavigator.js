fb8ac2594d5a5076ebe423359dcde9f1e190b98e
Jaime <ingeniero.arriola@gmail.com>
2021-05-13 04:28:02 -0600

    Added bottom tabs navigation

--- navigation/TechsNavigator.js
+++ navigation/TechsNavigator.js
@@ -1,12 +1,24 @@
+import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer  } from 'react-navigation'
+import {Platform} from 'react-native'
 import { createBottomTabNavigator  } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import Colors from '../constants/Colors'
 import FavoritesScreen from '../Screens/FavoritesScreen'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from '../Screens/CategoriesScreen'
 import CategoriesTechsScreen from '../Screens/CategoriesTechsScreen'
 import TechDetailScreen from '../Screens/TechDetailScreen'
+
+const navOptions = {
+    headerStyle:{
+        backgroundColor:Colors.primaryColor
+        },
+        headerTintColor: "white",
+        headerTitleAlign:'center' 
+}
 
 const TechsNavigator = createStackNavigator({
     Categories:{
@@ -16,20 +28,63 @@ const TechsNavigator = createStackNavigator({
     TechDetail:TechDetailScreen 
     },
     {
-        defaultNavigationOptions:{
-            headerStyle:{
-                backgroundColor:Colors.primaryColor
-                },
-                headerTintColor: "white",
-                headerTitleAlign:'center' 
-        }
+        defaultNavigationOptions: navOptions
     }
 )
 
-
-const BottomNavigator = createBottomTabNavigator({
-    Techs:TechsNavigator,
-    Favorites : FavoritesScreen
+const FavoritesNavigator = createStackNavigator({
+    FavoritesScreen:FavoritesScreen,
+    TechDetail:TechDetailScreen 
+},
+{
+    defaultNavigationOptions: navOptions
 })
+
+const TabConfiguration = {
+    Techs:{
+        screen:TechsNavigator,
+        navigationOptions:{
+            tabBarLabel:'Techs',//This is up to you becasue by default values are the key of this objetc, I mean Techs here
+            tabBarIcon:(tabInfo) => {
+                return <Ionicons 
+                        name='code'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+            },
+            tabBarColor:Colors.primaryColor
+        }
+    },
+    Favorites : {
+        screen:FavoritesNavigator,
+        navigationOptions:{
+            tabBarLabel:'Favorites',
+            tabBarIcon:(tabInfo) => {
+                return <Ionicons 
+                        name='star'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+            },
+            tabBarColor:Colors.accentColor
+        }
+    }
+}
+
+const BottomNavigator = Platform.OS === "android" 
+    ? createMaterialBottomTabNavigator(TabConfiguration,{
+        activeColor:'#fff',
+        shifting:true
+        /* 
+        barStyle:{
+            backgroundColor: Colors.primaryColor
+        }
+        */
+    }) 
+    : createBottomTabNavigator(TabConfiguration,{
+        tabBarOptions: {
+            activeTintColor:Colors.accentColor
+        }
+    })
 
 export default createAppContainer(BottomNavigator) 
