0e353316df44631b32b20f0f803a7be2910dc620
Jaime <ingeniero.arriola@gmail.com>
2021-05-13 17:24:56 -0600

    Added navigation drawer

--- Screens/FiltersScreen.js
+++ Screens/FiltersScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {View,Text,StyleSheet} from 'react-native'
+import { HeaderButtons, Item  } from 'react-navigation-header-buttons'
+import CustomHButon from '../components/CustomHButon'
 
-const FiltersScreen = () => {
+const FiltersScreen = (props) => {
     return (
         <View style={styles.screen} >
             <Text>
@@ -9,6 +11,20 @@ const FiltersScreen = () => {
             </Text>
         </View>
     )
+}
+
+FiltersScreen.navigationOptions = navData => {
+    return {  
+        headerTitle : 'Filtered Techs',
+        headerLeft: <HeaderButtons HeaderButtonComponent={CustomHButon} >
+        <Item
+            iconName='menu'
+            onPress={()=>{
+                navData.navigation.toggleDrawer()
+            }}
+        />
+    </HeaderButtons>
+    }
 }
 
 const styles = StyleSheet.create({
