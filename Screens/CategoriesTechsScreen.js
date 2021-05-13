8335e31063e901211ff60a072f6e707150fba5af
Jaime <ingeniero.arriola@gmail.com>
2021-05-13 17:22:53 -0600

    removed techlist and creted a reusable component

--- Screens/CategoriesTechsScreen.js
+++ Screens/CategoriesTechsScreen.js
@@ -1,40 +1,16 @@
 import React from 'react'
-import {View,Text,StyleSheet,Button,FlatList} from 'react-native'
 import {TECHS} from '../data/data'
-import Tech from '../models/Tech'
-import TechItem from '../components/TechItem'
+import TechList from '../components/TechList'
 
 const CategoriesTechsScreen = (props) => {
     const itemPassed=props.navigation.getParam('item')
     const techsToDisplay =  TECHS.filter(tech => tech.category.indexOf(itemPassed.id)>=0)
 
-    const renderTechItem = (itemData) => {
-        console.log(itemData.item.image)
-        return(
-            <TechItem 
-            name={itemData.item.name}
-            image={itemData.item.image}
-            onSelectTech={()=>{
-                props.navigation.navigate({
-                    routeName:'TechDetail',
-                    params:{
-                        item:itemData.item
-                    }
-                })
-            }}
-            />
-        )
-    }
-
     return (
-        <View style={styles.screen} >
-            <FlatList
-                data={techsToDisplay}
-                keyExtractor={(item,index) => item.id}
-                renderItem={renderTechItem}
-                style={{width:'100%'}}
-            />
-        </View>
+        <TechList 
+        techsToDisplay={techsToDisplay}
+        navigation={props.navigation}
+        />
     )
 }
 
@@ -44,15 +20,6 @@ CategoriesTechsScreen.navigationOptions = navigationData => {
         headerTitle : itemPassed.title,
     }
 }
-
-
-const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
 
 export default CategoriesTechsScreen
 
