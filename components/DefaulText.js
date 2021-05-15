053f8749cb3b19d58ec731f1bc66797b3876790a
Jaime <ingeniero.arriola@gmail.com>
2021-05-15 10:45:37 -0600

    component to show text with open-sans fontFamily

---
+++ components/DefaulText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const DefaulText = (props) => {
+    return (
+        <Text style={{fontFamily='open-sans'}} >
+            {props.children}
+        </Text>
+    )
+}
+
+export default DefaulText
