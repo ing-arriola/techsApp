189ad74692e914041c58d45a8fa2a3cdf53532a2
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-16 12:18:41 -0600

    Added action for toggle favorites

--- store/reducers/techs.js
+++ store/reducers/techs.js
@@ -1,4 +1,5 @@
 import {TECHS} from '../../data/data'
+import {TOGGLE_FAVORITES} from '../actions/techs'
 const initialState = {
     techs:TECHS,
     filteredTechs:TECHS,
@@ -6,6 +7,19 @@ const initialState = {
 }
 
 const techsReducer = (state=initialState,action) => {
+    switch(action.type){
+        case TOGGLE_FAVORITES:
+            const existingIndex = state.favoritesTechs.findIndex(tech => tech.id === action.tech.id)
+            console.log(existingIndex)
+            if(existingIndex>=0){
+                const updatedFavorites = [...state.favoritesTechs]
+                updatedFavorites.splice(existingIndex,1)
+                return {...state,favoritesTechs:updatedFavorites}
+            }
+            else{
+                return {...state,favoritesTechs:[...state.favoritesTechs,action.tech]}
+            }
+    }
     return state
 }
 
