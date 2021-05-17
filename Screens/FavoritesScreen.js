1b0ab0fec43b223428ce32f3ba39bc756737767c
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-17 09:59:32 -0600

    Added empty state

--- Screens/FavoritesScreen.js
+++ Screens/FavoritesScreen.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {View,StyleSheet} from 'react-native'
 import TechList from '../components/TechList'
 import { HeaderButtons, Item  } from 'react-navigation-header-buttons'
 import { useSelector } from 'react-redux'
 import CustomHButon from '../components/CustomHButon'
+import Empty from '../assets/empty.svg'
+import DefaultText from '../components/DefaulText'
 
 
 
@@ -10,12 +13,22 @@ import CustomHButon from '../components/CustomHButon'
 const FavoritesScreen = (props) => {
     const availableTechs = useSelector(state => state.techs.favoritesTechs)
 
-    
+    if(availableTechs.length === 0){
+        return(
+            <View style={styles.emptyContainer} >
+                <Empty width={300} height={200} />
+                <DefaultText>
+                    There are not Favorites yet, please add some favorites :)
+                </DefaultText>
+            </View>
+        )
+    }
+
     return (
         <TechList 
-        techsToDisplay={availableTechs}
-        navigation={props.navigation}
-        />
+            techsToDisplay={availableTechs}
+            navigation={props.navigation}
+            />
     )
 }
 
@@ -32,5 +45,13 @@ FavoritesScreen.navigationOptions = navData => {
     </HeaderButtons>
     }
 }
+
+const styles=StyleSheet.create({
+    emptyContainer:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
 
 export default FavoritesScreen
