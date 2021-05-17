01c0585e48d3e6136fdac4f5d3891711dab30d95
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-17 10:03:56 -0600

    changed the way to add a new item

--- store/reducers/techs.js
+++ store/reducers/techs.js
@@ -10,14 +10,13 @@ const techsReducer = (state=initialState,action) => {
     switch(action.type){
         case TOGGLE_FAVORITES:
             const existingIndex = state.favoritesTechs.findIndex(tech => tech.id === action.tech.id)
-            console.log(existingIndex)
             if(existingIndex>=0){
                 const updatedFavorites = [...state.favoritesTechs]
                 updatedFavorites.splice(existingIndex,1)
                 return {...state,favoritesTechs:updatedFavorites}
             }
             else{
-                return {...state,favoritesTechs:[...state.favoritesTechs,action.tech]}
+                return {...state,favoritesTechs: state.favoritesTechs.concat(action.tech)}
             }
     }
     return state
